a1d038b2e43bb57f4a80c6984a2622d81743dee3
seb5g <sebastien.weber@cemes.fr>
2025-11-21 11:07:18 +0100

    cannot access display for time that is a time object!

--- src/fullcoster/growthimp/models.py
+++ src/fullcoster/growthimp/models.py
@@ -75,13 +75,11 @@ class Record(*base_class):
         try:
             if hasattr(self, 'date_to'):
                 return (f'{activity_short} record {self.id} submitted the {sub}: {self.user} used '
-                        f'{self.experiment} from {self.date_from}/{self.get_time_from_display()} to'
-                        f' {self.date_to}/{self.get_time_to_display()}')
+                        f'{self.experiment} from {self.date_from}/{self.time_from.isoformat()} to'
+                        f' {self.date_to}/{self.time_to.isoformat()}')
             else:
                 return (f'{activity_short} record {self.id} submitted the {sub}: {self.user} used '
-                        f'{self.experiment} the {self.date_from}/{self.get_time_from_display()} to'
-                        f' {self.date_from}/{self.get_time_to_display()}')
+                        f'{self.experiment} the {self.date_from}/{self.time_from.isoformat()} to'
+                        f' {self.date_from}/{self.time_to.isoformat()}')
         except:
             return str(self)
-
-
